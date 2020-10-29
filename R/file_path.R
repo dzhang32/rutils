ad8a86a2a0a53cd01244a9b7fe5ca1d0ca71561d
David Zhang <david.zhang.12@ucl.ac.uk>
2020-10-29 13:15:34 +0000

    file_path - add with testing

---
+++ R/file_path.R
@@ -0,0 +1,40 @@
+
+#' Create a file path and/or check if it exists
+#'
+#' Creates a file path and also can check/force it to exists in one step. Used
+#' mostly in conjunction with `here::here`, for checking whether a file exists
+#' (you've typed the path correctly).
+#'
+#' @param ... passed to `base::file.path`.
+#' @param check_exists `logical(1)` specifying whether to print the path and if
+#'   it exists locally.
+#' @param force_exists `logical(1)` specifying whether to return an error if the
+#'   path does not exist.
+#'
+#' @return file path
+#' @export
+#'
+#' @examples
+#'
+#' file_path("/")
+file_path <- function(..., check_exists = TRUE, force_exists = FALSE) {
+    path <- file.path(...)
+
+    if (check_exists) {
+        message(paste0(
+            path,
+            ifelse(file.exists(path), " exists", " does not exist")
+        ))
+    }
+
+    if (force_exists) {
+        if (!file.exists(path)) {
+            stop(paste0(
+                path,
+                " does not exist"
+            ))
+        }
+    }
+
+    return(path)
+}
---
+++ tests/testthat/test-file_path.R
@@ -0,0 +1,15 @@
+context("test file_path")
+
+test_that("file_path has correct output", {
+    expect_equal(
+        file_path("/", check_exists = FALSE),
+        "/"
+    )
+    expect_equal(
+        file_path("~", check_exists = FALSE),
+        "~"
+    )
+    expect_message(file_path("/"), "exists")
+    expect_message(file_path("/non/existant/path"), "does not exist")
+    expect_error(file_path("/non/existant/path", force_exists = TRUE))
+})
