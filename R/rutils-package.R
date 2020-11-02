40c7dfd35df242d7220a8a5d0c80b830ddd3a5b7
David Zhang <david.zhang.12@ucl.ac.uk>
2020-11-02 12:23:25 +0000

    Add rutils-package.R for importFrom

---
+++ R/rutils-package.R
@@ -0,0 +1,10 @@
+#' rutils: home for commonly used R utility functions
+#'
+#' Placeholder for package description - to be updated
+#'
+#' @docType package
+#' @name rutils
+NULL
+
+#' @importFrom magrittr %>%
+NULL
