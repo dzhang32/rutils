a9838f278fc09fcbd2e1b4c32a22ffe933b1b7f0
David Zhang <david.zhang.12@ucl.ac.uk>
2020-11-03 13:18:07 +0000

    docker_run_rserver() - add option for non read-only volumes

--- R/docker_shell.R
+++ R/docker_shell.R
@@ -11,9 +11,13 @@
 #' @param name `character(1)` name for the container.
 #' @param rm `logical(1)` whether to use the `-rm` flag when running the
 #'   container. Should the container be removed when stopped?
-#' @param volumes `character(1)` paths for the hosts files that you want
+#' @param volumes_ro `character(1)` paths for the hosts files that you want
 #'   accessible in the container. These will be mounted using the identical
 #'   paths on the host and with `read-only` permissions.
+#' @param volumes `character(1)` paths for the hosts files that you want
+#'   accessible in the container. These will NOT be mounted with `read-only`
+#'   permissions. Use this option with care, recommended only for directories
+#'   that you want to output results into.
 #'
 #' @return NULL
 #' @export
@@ -25,13 +29,13 @@
 #'
 #' \dontrun{
 #' docker_run_rserver()
-#' docker_run_rserver(volumes = "/home/dzhang/projects/dasper_analysis/raw_data;/home/dzhang/projects/dasper_analysis/scripts")
 #' }
 docker_run_rserver <- function(image = "bioconductor/bioconductor_docker:devel",
     port = 8888,
     password = "bioc",
     name = "dz_bioc",
     rm = FALSE,
+    volumes_ro = NULL,
     volumes = NULL) {
 
     # set up the args for password and port
@@ -44,18 +48,49 @@ docker_run_rserver <- function(image = "bioconductor/bioconductor_docker:devel",
         cmdfun::cmd_list_interp() %>%
         cmdfun::cmd_list_to_flags(prefix = "--")
 
-    if (!is.null(volumes)) {
-        volumes <-
-            volumes %>%
-            stringr::str_c(., ":", .) %>%
-            stringr::str_c("-v ", ., ":ro") %>%
-            stringr::str_c(collapse = " ")
+    if(!is.null(volumes_ro)){
+
+        volumes_ro <-
+            volumes_ro %>%
+            .volumes_to_flag(read_only = TRUE)
     }
 
-    docker_cmd(c("run", docker_flags, volumes, image))
+    if(!is.null(volumes)){
+
+        volumes <-
+            volumes %>%
+            .volumes_to_flag(read_only = FALSE)
+
+    }
+
+    .docker_cmd(c("run", docker_flags, volumes_ro, volumes, image))
 }
 
 #' @noRd
-docker_cmd <- function(...) {
+.volumes_to_flag <- function(volumes, read_only = FALSE){
+
+    volumes_flag <-
+        volumes %>%
+        stringr::str_c(., ":", .) %>%
+        stringr::str_c("-v ", .)
+
+    if(read_only){
+
+        volumes_flag <-
+            volumes_flag %>%
+            stringr::str_c(., ":ro")
+
+    }
+
+    volumes_flag <-
+        volumes_flag %>%
+        stringr::str_c(collapse = " ")
+
+    return(volumes_flag)
+
+}
+
+#' @noRd
+.docker_cmd <- function(...) {
     system2("docker", ...)
 }
