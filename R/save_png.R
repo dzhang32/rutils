19344f6a66d8b1d28545834b1bac88bcf266b533
David Zhang <dyzhang32@gmail.com>
2021-09-19 15:18:32 +0100

    Switch to using native pipe for example

--- R/save_png.R
+++ R/save_png.R
@@ -1,14 +1,16 @@
 #' Save a plot as a .png
 #'
-#' A wrapper for calling `grDevices::png()` then `dev.off()`. Allows users to
-#' save a plot to a specified path. The preferred method for `ggplot2::ggplot`
-#' objects remains `ggplot::ggsave()`. However if you wish to use this function
-#' to save `ggplot2::ggplot` objects, print the plot as the last step of
-#' `plot_code`.
+#' A wrapper for calling `grDevices::png()` then `grDevices::dev.off()`. Allows
+#' users to save a plot to a specified path. The preferred method for
+#' `ggplot2::ggplot` objects remains `ggplot::ggsave()`. However if you wish to
+#' use this function to save `ggplot2::ggplot` objects, print the plot as the
+#' last step of `plot_code`.
 #'
 #' @inheritParams grDevices::png
-#' @param plot_code code required to generate the plot, sandwiched in curly braces.
-#' @param path `character(1)` specifying path for where to save your plot, including file name.
+#' @param plot_code code required to generate the plot, sandwiched in curly
+#'   braces.
+#' @param path `character(1)` specifying path for where to save your plot,
+#'   including file name.
 #' @param ... additional arguments are passed to `grDevices::png()`.
 #'
 #' @return Invisibly, the path to saved plot.
@@ -18,7 +20,7 @@
 #'
 #' {
 #'     plot(x = 1:5)
-#' } %>%
+#' } |>
 #'     save_png(
 #'         path = file.path(tempdir(), "test_plot.png"),
 #'     )
@@ -30,10 +32,10 @@ save_png <- function(plot_code,
     res = 600,
     ...) {
     on.exit(expr = {
-        dev.off()
+        grDevices::dev.off()
     }, add = TRUE)
 
-    png(
+    grDevices::png(
         filename = path,
         width = width,
         height = height,
